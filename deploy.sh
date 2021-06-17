c5306596ff9739770761d7aa14a5408048c24c70
Michael Malura <michael@malura.de>
2021-06-17 10:45:48 +0200

    Fix github actions

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 RELEASE=prod
 OUTPUT_DIR=_output
-HOST=malura_malura
+HOST=$1
 DESTINATION="/var/www/malura/${RELEASE}/"
 
 if ! rsync -ra --progress --delete "${OUTPUT_DIR}/" "${HOST}":"${DESTINATION}"; then exit 1; fi
 if ! rsync -ra --progress nginx.conf "${HOST}":"/etc/nginx/sites/malura.de.conf"; then exit 1; fi
 if ! rsync -ra --progress redirects.map "${HOST}":"${DESTINATION}"; then exit 1; fi
-ssh malura_malura "cd ${DESTINATION} && chown -R www-data:www-data ${DESTINATION} && chmod -R 755 ${DESTINATION} && systemctl reload nginx"
+ssh $HOST "cd ${DESTINATION} && chown -R www-data:www-data ${DESTINATION} && chmod -R 755 ${DESTINATION} && systemctl reload nginx"
