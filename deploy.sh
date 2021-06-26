79a375906d2394561785ce3a8628f574b3c8f434
Michael Malura <michael@malura.de>
2021-06-26 20:31:01 +0200

    Run stats generation on deployment

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,5 @@ DESTINATION="/var/www/malura/${RELEASE}/"
 
 if ! rsync -ra --progress --delete "${OUTPUT_DIR}/" "${HOST}":"${DESTINATION}"; then exit 1; fi
 if ! rsync -ra --progress nginx.conf "${HOST}":"/etc/nginx/sites/malura.de.conf"; then exit 1; fi
-if ! rsync -ra --progress generatestats "${HOST}":"/etc/cron.hourly/"; then exit 1; fi
 if ! rsync -ra --progress redirects.map "${HOST}":"${DESTINATION}"; then exit 1; fi
-ssh $HOST "cd ${DESTINATION} && chown -R www-data:www-data ${DESTINATION} && chmod -R 755 ${DESTINATION} && systemctl reload nginx && systemctl restart cron"
+ssh $HOST "cd ${DESTINATION} && chown -R www-data:www-data ${DESTINATION} && chmod -R 755 ${DESTINATION} && systemctl reload nginx && systemctl restart cron && ./generatestats"
