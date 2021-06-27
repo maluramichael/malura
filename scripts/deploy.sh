99b4111d920a164d41b523f61e2844b4bb6e0ee9
Michael Malura <michael@malura.de>
2021-06-27 13:31:59 +0200

    Fix deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+RELEASE=prod
+OUTPUT_DIR=_output
+HOST=$1
+DESTINATION="/var/www/malura/${RELEASE}/"
+
+if ! rsync -ra --progress --delete "${OUTPUT_DIR}/" "${HOST}":"${DESTINATION}"; then exit 1; fi
+if ! rsync -ra --progress nginx.conf "${HOST}:/etc/nginx/sites/malura.de.conf"; then exit 1; fi
+if ! rsync -ra --progress redirects.map "${HOST}":"${DESTINATION}"; then exit 1; fi
+if ! rsync -ra --progress generatestats "${HOST}:/etc/cron.daily/"; then exit 1; fi
+ssh $HOST "cd ${DESTINATION} && chown -R www-data:www-data ${DESTINATION} && chmod -R 755 ${DESTINATION} && systemctl reload nginx && systemctl restart cron"
