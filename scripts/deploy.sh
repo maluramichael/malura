af43585bca2f9f2d686df54a1e12fd11358be1e8
Michael Malura <michael@malura.de>
2022-02-20 17:50:04 +0100

    Update nginx site path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ HOST=$1
 DESTINATION="/var/www/malura/${RELEASE}/"
 
 if ! rsync -ra --progress --delete "${OUTPUT_DIR}/" "${HOST}":"${DESTINATION}"; then exit 1; fi
-if ! rsync -ra --progress nginx.conf "${HOST}:/etc/nginx/sites/malura.de.conf"; then exit 1; fi
+if ! rsync -ra --progress nginx.conf "${HOST}:/etc/nginx/sites-enabled/malura.de.conf"; then exit 1; fi
 if ! rsync -ra --progress redirects.map "${HOST}":"${DESTINATION}"; then exit 1; fi
 if ! rsync -ra --progress generatestats "${HOST}:/etc/cron.hourly/"; then exit 1; fi
 
